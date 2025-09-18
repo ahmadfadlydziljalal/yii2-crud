659102a1e2b7f4f0662415da5cc0c2046d6b1fe4
afdj <ahmadfadlydziljalal@gmail.com>
2025-09-18 17:22:06 +0700

    fixed: Bootstrap.php autoload

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -3,27 +3,25 @@
 namespace dzil\yii2_crud;
 
 use Yii;
-use yii\base\Application;
 use yii\base\BootstrapInterface;
 
-/**
- * @author Ahmad Fadly Dzil Jalal <ahmadfadlydziljalal@gmail.com>
- * @since 1.0
- */
-class Bootstrap implements BootstrapInterface {
-
-    /**
-     * Bootstrap method to be called during the application bootstrap stage.
-     * @param Application $app the application currently running
-     */
+class Bootstrap implements BootstrapInterface
+{
     public function bootstrap($app): void
     {
-       Yii::setAlias("@dzilcrud", __DIR__);
+        Yii::setAlias('@dzilcrud', __DIR__);
+
         if ($app->hasModule('gii')) {
-            if (!isset($app->getModule('gii')->generators['dzilcrud'])) {
-                $app->getModule('gii')->generators['dzilcrud'] = 'dzil\yii2_dzil_crud\generators\Generator';
+            $gii = $app->getModule('gii');
+            if (!isset($gii->generators['dzilcrud'])) {
+                $gii->generators['dzilcrud'] = [
+                    'class' => 'dzil\yii2_crud\generators\Generator',
+                    'templates' => [
+                        'default' => '@dzilcrud/generators/template/default',
+                        'ajax'    => '@dzilcrud/generators/template/ajax',
+                    ],
+                ];
             }
         }
     }
-
 }
